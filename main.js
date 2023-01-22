94cc692bfe5bc01c29f584cdb4b22dd305b60fca
Felix S <116848884+latsonj@users.noreply.github.com>
2023-01-22 22:53:51 +1300

    Create endMessage() function to show game result

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Return 3 string outputs from RNG(1, 3) to plug in playRound() function.
 function getComputerChoice() {
   let randNum = Math.floor(Math.random() * 3) + 1;
+
   switch (randNum) {
     case 1:
       return "Rock";
@@ -16,18 +17,25 @@ function getComputerChoice() {
 
 //Takes 2 parameters and compares them to output win/loss string.
 function playRound(playerSelection, computerSelection) {
+
   if (playerSelection === "paper" && computerSelection === "rock") {
     return "Computer chose rock. You won this round! :)";
+
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     return "Computer chose scissors. You won this round! :)";
+
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     return "Computer chose paper. You won this round! :)";
+
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     return "Computer chose paper. Sorry, you lose this round! :(";
+
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     return "Computer chose scissors. Sorry, you lose this round! :(";
+
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     return "Computer chose rock. Sorry, you lose this round! :(";
+
   } else if (playerSelection === computerSelection) {
     return "Computer chose the same. It's a tie!";
   }
@@ -47,17 +55,40 @@ function game() {
   for (let i = 0; i < 5; i++) {
     let playerSelection = prompt("Enter \"rock\", \"paper\" or \"scissors\": ").toLowerCase();
     let computerSelection = getComputerChoice().toLowerCase();
+
     if (playRound(playerSelection, computerSelection).includes(smiley)) {
+
       console.log(playRound(playerSelection, computerSelection));
       console.log(`Player: ${++playerScore}     Computer: ${computerScore}`);
+
     } else if (playRound(playerSelection, computerSelection).includes(frown)) {
+
       console.log(playRound(playerSelection, computerSelection));
       console.log(`Player: ${playerScore}     Computer: ${++computerScore}`);
+
     } else if (playRound(playerSelection, computerSelection).includes(tie)) {
+
       console.log(playRound(playerSelection, computerSelection));
       console.log(`Player: ${playerScore}     Computer: ${computerScore}`)
+
     }
   }
 }
 
+//Compares scores, outputs message when game function ends.
+function endMessage() {
+
+  if (playerScore > computerScore) {
+    console.log("You have defeated the computer!")
+
+  } else if (playerScore < computerScore) {
+    console.log("The computer has defeated you!")
+
+  } else {
+    console.log("Draw game!")
+  }
+}
+
+//Invoking functions.
 game();
+endMessage();
